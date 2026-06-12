82100497f23f0b2927690d94b74d0c0f57880a21
Renate Roke <renateroke@proton.me>
2026-06-12 07:16:00 +0200

    setup react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+    <h1>🚧 Wordt vernieuwd</h1>
+    <p>Binnenkort iets nieuws hier.</p>
+  </div>
+    </>
+  )
+}
+
+export default App
