3dc6c0f492248f2e50b2c935e38ac4c624021e98
Renate Roke <renateroke@proton.me>
2026-06-20 12:40:23 +0200

    Over mij page title

--- src/pages/OverMij.jsx
+++ src/pages/OverMij.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const OverMij = () => {
     return (
         <div>
+            <title>Over mij - Renate Roke</title>
             <h1>🚧 Wordt vernieuwd</h1>
             <p>Binnenkort iets nieuws hier.</p>
         </div>
