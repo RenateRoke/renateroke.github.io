e438f223a19287145bf79cd5948d9b23748eea64
Renate Roke <renateroke@proton.me>
2026-06-21 17:22:44 +0200

    Focusmanagement en kleine kleur-rework

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import { useRef } from 'react';
 import SkipLink from '../SkipLink/SkipLink.jsx';
 import './NavBar.css';
 
 const NavBar = () => {
+  const navRef = useRef(null);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      if (navRef.current) {
+        navRef.current.focus();
+      }
+    }, 0);
+  }
+
   return (
-    <header className="navbar">
+    <header tabindex="-1" ref={navRef} className="navbar">
       <span className="name">Renate Roke</span>
       <nav>
         <ul role="list">
           <li><SkipLink /></li>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/overmij">Over mij</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
+          <li><NavLink onClick={handleClick} reloadDocument to="/">Home</NavLink></li>
+          <li><NavLink onClick={handleClick} reloadDocument to="/overmij">Over mij</NavLink></li>
+          <li><NavLink onClick={handleClick} reloadDocument to="/contact">Contact</NavLink></li>
           <li><a href="/rpg">RPG</a></li>
         </ul>
       </nav>
